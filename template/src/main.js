27fb1f4ffca5e5bd883eff0d74393dd19b477c36
Daksh Miglani <hello@dak.sh>
2018-01-26 19:39:37 +0530

    import vuetify before app

--- template/src/main.js
+++ template/src/main.js
@@ -1,7 +1,7 @@
 import Vue from 'vue';
-import App from './App.vue';
 import Vuetify from 'vuetify';
 import 'vuetify/dist/vuetify.min.css';
+import App from './App.vue';
 import router from './router';
 
 Vue.use(Vuetify);
