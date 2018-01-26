008a358f70900847049b615f4e3af66b4c4c057a
Daksh Miglani <hello@dak.sh>
2018-01-26 19:23:51 +0530

    fix @

--- template/src/components/HelloWorld.vue
+++ template/src/components/HelloWorld.vue
@@ -2,9 +2,9 @@
   <v-container fluid>
     <v-slide-y-transition mode="out-in">
       <v-layout column align-center>
-        <img src="@/assets/logo.png" alt="Vuetify.js" class="mb-5">
+        <img src="../assets/logo.png" alt="Vuetify.js" class="mb-5">
         <blockquote>
-          &#8220;First, solve the problem. Then, write the code.&#8221;
+          <i>First, solve the problem. Then, write the code.</i>
           <footer>
             <small>
               <em>&mdash;John Johnson</em>
