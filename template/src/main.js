35cfd7c746a134e7b4e4b510c0cce80b28c09167
Daksh Miglani <hello@dak.sh>
2018-01-26 20:08:15 +0530

    router, runtime and standalone

--- template/src/main.js
+++ template/src/main.js
@@ -2,7 +2,9 @@ import Vue from 'vue';
 import Vuetify from 'vuetify';
 import 'vuetify/dist/vuetify.min.css';
 import App from './App.vue';
-import router from './router';
+{{#router}}
+import router from './router'
+{{/router}}
 
 Vue.use(Vuetify);
 
@@ -11,6 +13,14 @@ Vue.config.productionTip = false;
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
+  {{#router}}
   router,
+  {{/router}}
+  {{#if_eq build "runtime"}}
   render: h => h(App)
+  {{/if_eq}}
+  {{#if_eq build "standalone"}}
+  components: { App },
+  template: '<App/>'
+  {{/if_eq}}
 });
